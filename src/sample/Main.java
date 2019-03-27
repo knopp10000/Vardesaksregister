d7f66256b86a47e2b72b3cd4d08beb2e07c00b74
knopp10000 <karl.gustafsson@live.com>
2019-03-27 14:58:48 +0100

    did some stuff

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,21 +1,61 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import javafx.stage.Window;
+
 
 public class Main extends Application {
 
+    Stage window;
+    Scene scene1, scene2;
+    Button button1, button2, button3;
+    TextArea textArea;
+    Label label;
+
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        window = primaryStage;
+        primaryStage.setTitle("Värdesaksregister");
+        textArea = new TextArea();
+        label = new Label();
+        label.setText("Rawrrrrrrrrrrrrrrrrrrrrrrrrrrrr");
+        button1 = new Button("Go to scene 2!");
+        button1.setOnAction(e -> window.setScene(scene2));
+        button2 = new Button("Go to scene 1");
+        button2.setOnAction(e -> window.setScene(scene1));
+        button3 = new Button();
+        button3.setOnAction(event -> AlertBox.display("test","This is a test!"));
+
+        StackPane layout1 = new StackPane();
+        layout1.getChildren().addAll(label, button1);
+        StackPane layout2 = new StackPane();
+        layout2.getChildren().addAll(textArea, button2, button3);
+
+        scene1 = new Scene(layout1, 300, 250);
+        scene2 = new Scene(layout2, 300, 250);
+        primaryStage.setScene(scene1);
         primaryStage.show();
     }
 
+/*    @Override
+    public void handle(ActionEvent event) {
+        if (event.getSource() == button){
+            System.out.println("WOW!");
+        }else{
+            System.out.println("Meh!");
+        }
+
+    }*/
 
     public static void main(String[] args) {
         launch(args);
